f0b8455ea725b015d943a2aef24f1d478e9830a4
sven <bohms@ideomatic.io>
2021-07-08 20:11:02 -0400

    if we declare floats the initial values should be floats

--- lib/licor.ex
+++ lib/licor.ex
@@ -2,8 +2,8 @@ defmodule Licor do
   @moduledoc """
   Licor data struct
   """
-  defstruct source: :licor, datetime: DateTime.utc_now(), co2: 0, temperature: 0,
-    pressure: 0, co2_abs: 0, ivolt: 0, raw: 0
+  defstruct source: :licor, datetime: DateTime.utc_now(), co2: 0.0, temperature: 0.0,
+    pressure: 0.0, co2_abs: 0.0, ivolt: 0.0, raw: 0.0
 
   @typedoc """
   A custom type that holds the data from the licor
